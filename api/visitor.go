69301ab174b0e87a5d9c174dc9d1f818be235af8
hubqin <wuhanqinb@gmail.com>
2025-10-08 05:56:34 +0800

    add visitor count formatting (k/M notation)

--- api/visitor.go
+++ api/visitor.go
@@ -9,6 +9,24 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+func formatNumber(num int64) string {
+	if num >= 1000000 {
+		millions := float64(num) / 1000000.0
+		if millions == float64(int64(millions)) {
+			return fmt.Sprintf("%.0fM", millions)
+		}
+		return fmt.Sprintf("%.1fM", millions)
+	}
+	if num >= 1000 {
+		thousands := float64(num) / 1000.0
+		if thousands == float64(int64(thousands)) {
+			return fmt.Sprintf("%.0fk", thousands)
+		}
+		return fmt.Sprintf("%.1fk", thousands)
+	}
+	return fmt.Sprintf("%d", num)
+}
+
 func Visitor(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -26,10 +44,12 @@ func Visitor(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	
+	formattedCount := formatNumber(count)
+	
 	response := fmt.Sprintf(`
-		<div class="stat-number">%d</div>
+		<div class="stat-number">%s</div>
 		<div class="stat-label">Visitors</div>
-	`, count)
+	`, formattedCount)
 	
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(response))
